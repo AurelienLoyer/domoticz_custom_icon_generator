b3d2574958cb9d00942f72c93095354efc058c8c
Aurélien Loyer <aur.loy@gmail.com>
2024-03-17 19:12:44 +0100

    chore: migrate to Angular 17 (part.1) 🚀

---
+++ angular-17/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgIf],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'domoticz_custom_icon_generator';
+}
