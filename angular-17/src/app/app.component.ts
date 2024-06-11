3c71c723e21566553ce2bc8c4286dde0959eeae6
Aurélien Loyer <aur.loy@gmail.com>
2024-06-11 13:47:02 +0200

    feat: add ssr and split step 2 in async load ✨

--- angular-17/src/app/app.component.ts
+++ angular-17/src/app/app.component.ts
@@ -1,75 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { ImageCropperComponent, ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
+import { Step2Component } from './steps/step-2/step-2.component';
 
 type OnOff = 'on' | 'off';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, ImageCropperComponent, FormsModule],
+  imports: [RouterOutlet, NgIf, FormsModule, Step2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  public imageOnChangedEvent: Event | null = null;
-  public imageOffChangedEvent: Event | null = null;
-  public croppedImageOn: SafeUrl = '';
   public croppedImageOnBlob: Blob | undefined | null = undefined;
-  public croppedImageOff: SafeUrl = '';
   public croppedImageOffBlob: Blob | undefined | null = undefined;
+  public isAppLoaded = false;
   public iconName = '';
 
-  constructor(
-    private sanitizer: DomSanitizer
-  ) {
+  public ngAfterViewInit() {
+    this.isAppLoaded = true;
   }
 
-  public fileChangeEvent(event: Event, onOff: OnOff): void {
+  public assignCropperImageBlob(blob: Blob, onOff: OnOff) {
     if (onOff === 'on') {
-      this.imageOnChangedEvent = event;
+      this.croppedImageOnBlob = blob;
     }
     else {
-      this.imageOffChangedEvent = event;
+      this.croppedImageOffBlob = blob;
     }
   }
-
-  public imageCropped(event: ImageCroppedEvent, onOff: OnOff) {
-    if (!event.objectUrl) {
-      console.log(event);
-
-      return;
-    }
-
-    if (onOff === 'on') {
-      this.croppedImageOn = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
-      this.croppedImageOnBlob = event.blob;
-    }
-    else {
-      this.croppedImageOff = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
-      this.croppedImageOffBlob = event.blob;
-    }
-    // event.blob can be used to upload the cropped image
-  }
-
-  imageLoaded(image: LoadedImage) {
-    // show cropper
-  }
-
-  cropperReady() {
-    // cropper ready
-  }
-
-  loadImageFailed() {
-    // show message
-  }
-
 
   public zip() {
 
---
+++ angular-17/src/app/steps/step-2/step-2.component.ts
@@ -0,0 +1,70 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ImageCropperComponent, ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+type OnOff = 'on' | 'off';
+
+@Component({
+    selector: 'app-step-2',
+    standalone: true,
+    imports: [NgIf, ImageCropperComponent],
+    templateUrl: './step-2.component.html',
+    styleUrl: './step-2.component.scss'
+})
+export class Step2Component {
+
+    public imageOnChangedEvent: Event | null = null;
+    public imageOffChangedEvent: Event | null = null;
+    public croppedImageOn: SafeUrl = '';
+    public croppedImageOff: SafeUrl = '';
+
+    @Output() public assignCropperImageOnBlob: EventEmitter<Blob> = new EventEmitter();
+    @Output() public assignCropperImageOffBlob: EventEmitter<Blob> = new EventEmitter();
+
+    constructor(
+        private sanitizer: DomSanitizer
+    ) {
+    }
+
+    public fileChangeEvent(event: Event, onOff: OnOff): void {
+        if (onOff === 'on') {
+            this.imageOnChangedEvent = event;
+        }
+        else {
+            this.imageOffChangedEvent = event;
+        }
+    }
+
+    public imageCropped(event: ImageCroppedEvent, onOff: OnOff) {
+        if (!event.objectUrl) {
+            console.log(event);
+
+            return;
+        }
+
+        if (onOff === 'on' && event.blob) {
+            this.croppedImageOn = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
+            this.assignCropperImageOnBlob.emit(event.blob);
+        }
+        else if (onOff === 'off' && event.blob) {
+            this.croppedImageOff = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
+            this.assignCropperImageOffBlob.emit(event.blob);
+        } else {
+            console.log(event);
+        }
+        // event.blob can be used to upload the cropped image
+    }
+
+    imageLoaded(image: LoadedImage) {
+        // show cropper
+    }
+
+    cropperReady() {
+        // cropper ready
+    }
+
+    loadImageFailed() {
+        // show message
+    }
+}
